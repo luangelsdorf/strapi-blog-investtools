8b7a4b6bb3285b253767f9dd7c3ec0687d2765da
luangelsdorf <luanferreira2136@gmail.com>
2021-09-26 22:23:22 -0300

    Added env URL

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('API_URL', '127.0.0.1'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
